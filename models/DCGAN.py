1eb3316f69bf8e0a3cc61e8d23e09213bfaddc70
Muhammad Momin Salman <muhammad.salman@fau.de>
2024-09-16 01:28:51 +0500

    Revert changes in train file

--- models/DCGAN.py
+++ models/DCGAN.py
@@ -23,12 +23,12 @@ class Discriminator(nn.Module):
     def __init__(self):
         super(Discriminator, self).__init__()
         self.model = nn.Sequential(
-            nn.Conv2d(1, 64, 4, 2, 1, bias=False),
+            nn.Conv2d(1, 64, 4, 2, 1, bias=False),      #bsx64x14x14
             nn.LeakyReLU(0.2, inplace=True),
-            nn.Conv2d(64, 128, 4, 2, 1, bias=False),
+            nn.Conv2d(64, 128, 4, 2, 1, bias=False),    #bsx128x7x7
             nn.BatchNorm2d(128),
             nn.LeakyReLU(0.2, inplace=True),
-            nn.Conv2d(128, 1, 7, 1, 0, bias=False),
+            nn.Conv2d(128, 1, 7, 1, 0, bias=False),     #bsx1x1x1
             nn.Sigmoid()
         )
 
--- train_MNIST.py
+++ train_MNIST.py
@@ -1,19 +1,16 @@
-import os
 import torch
 import torch.nn as nn
 import torch.optim as optim
 from torchvision import datasets, transforms
 from torch.utils.data import DataLoader
-from models import DCGAN
-from torch.utils.tensorboard import SummaryWriter
-import torchvision.utils as vutils
+import matplotlib.pyplot as plt
+from models import simpleGAN
+
 # Hyperparameters
 latent_size = 64
 batch_size = 100
 learning_rate = 0.0002
 epochs = 100
-log_dir = './runs/gan_mnist'
-device = "cuda"
 
 # Data loading
 transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,))])
@@ -21,33 +18,23 @@ mnist = datasets.MNIST(root='./data', train=True, transform=transform, download=
 dataloader = DataLoader(mnist, batch_size=batch_size, shuffle=True)
 
 # Models
-generator = DCGAN.Generator(latent_size).to(device)
-discriminator = DCGAN.Discriminator().to(device)
-
-
+generator = simpleGAN.Generator(latent_size)
+discriminator = simpleGAN.Discriminator()
 
 # Loss and optimizer
 criterion = nn.BCELoss()
 optimizer_g = optim.Adam(generator.parameters(), lr=learning_rate)
 optimizer_d = optim.Adam(discriminator.parameters(), lr=learning_rate)
 
-# TensorBoard summary writer
-if not os.path.exists(log_dir):
-    os.makedirs(log_dir)
-writer = SummaryWriter(log_dir=log_dir)
-
-# Fixed noise for consistent image generation
-fixed_noise = torch.randn(batch_size, latent_size).to(device)
-
 # Training loop
 for epoch in range(epochs):
     for i, (images, _) in enumerate(dataloader):
         # Flatten images
-        images = images.to(device)
+        images = images.view(-1, 28*28)
 
         # Create labels
-        real_labels = torch.ones_like(images)
-        fake_labels = torch.zeros_like(images)
+        real_labels = torch.ones(batch_size, 1)
+        fake_labels = torch.zeros(batch_size, 1)
 
         # Train discriminator
         optimizer_d.zero_grad()
@@ -55,7 +42,7 @@ for epoch in range(epochs):
         d_loss_real = criterion(outputs, real_labels)
         d_loss_real.backward()
 
-        z = torch.randn(batch_size, latent_size).to(device)
+        z = torch.randn(batch_size, latent_size)
         fake_images = generator(z)
         outputs = discriminator(fake_images.detach())
         d_loss_fake = criterion(outputs, fake_labels)
@@ -69,25 +56,4 @@ for epoch in range(epochs):
         g_loss.backward()
         optimizer_g.step()
 
-    # Log losses to TensorBoard
-    d_loss = d_loss_real + d_loss_fake
-    writer.add_scalar('Loss/Discriminator', d_loss.item(), epoch)
-    writer.add_scalar('Loss/Generator', g_loss.item(), epoch)
-
-    # Log losses to TensorBoard
-    d_loss = d_loss_real + d_loss_fake
-    writer.add_scalar('Loss/Discriminator', d_loss.item(), epoch)
-    writer.add_scalar('Loss/Generator', g_loss.item(), epoch)
-
-    # Log generated images to TensorBoard
-    if epoch % 5 == 0:  # Log images every 10 epochs
-        with torch.no_grad():
-            fake_images = generator(fixed_noise).view(-1, 1, 28, 28)  # Reshape to (batch_size, channels, height, width)
-            img_grid = vutils.make_grid(fake_images, normalize=True)
-            writer.add_image('Generated Images', img_grid, epoch)
-
-
     print(f'Epoch [{epoch}/{epochs}], d_loss: {d_loss_real + d_loss_fake}, g_loss: {g_loss}')
-
-# Close the writer after training
-writer.close()
